c078c6b509c4887025e16d0750eda1fba0fa0d46
Software <55256368+Soft-Devel@users.noreply.github.com>
2021-01-08 21:20:26 -0800

    shop choice upgrade

--- assets/js/game.js
+++ assets/js/game.js
@@ -77,15 +77,16 @@ var fightOrSkip = function() {
         window.alert(playerInfo.name + " has decided to skip this fight.  Goodbye!");
         // subtract money from playerMoney for skipping, but don't let them go into negative
         playerInfo.money = Math.max(0, playerInfo.money - 10);
+        shop();
 
         // return true if player wants to leave
         return true;
-      //  shop();
+        
       }
     } else {
       return false;
     }
-  }
+   }
 
 
 
@@ -164,21 +165,19 @@ var fight = function(enemy) {
 var shop = function() {
   // ask player what they'd like to do
   var shopOptionPrompt = window.prompt(
-    'Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one "REFILL", "UPGRADE", or "LEAVE" to make a choice.'
+    'Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one 1 for "REFILL", 2 for "UPGRADE", or 3 for "LEAVE".'
   );
 
   // use switch case to carry out action
+  shopOptionPrompt = parseInt(shopOptionPrompt);
   switch (shopOptionPrompt) {
-    case 'refill':
-    case 'REFILL':
+    case 1:
       playerInfo.refillHealth();
       break;
-    case 'upgrade':
-    case 'UPGRADE':
+    case 2:
       playerInfo.upgradeAttack();
       break;
-    case 'leave':
-    case 'LEAVE':
+    case 3:
       window.alert('Leaving the store.');
       break;
     default:
